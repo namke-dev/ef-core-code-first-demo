0bbf6a8974741d0aa044f3d21de05850ae2cb894
namke-dev <namke.dev@gmail.com>
2024-01-17 00:32:08 +0700

    implement CRUD

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -11,19 +11,62 @@ namespace SuperHeroAPI.Controllers
     [Route("api/[controller]")]
     public class SupperHeroController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAllSuperHero()
-        {
-            var superHero = new SuperHero
+        private static List<SuperHero> superHeroes = new List<SuperHero> {
+            new SuperHero
             {
                 Id = 1,
                 Name = "Spider Man",
                 FirstName = "Peater",
                 LastName = "Packer",
                 Place = "New York city"
-            };
+            },
+            new SuperHero
+            {
+                Id = 2,
+                Name = "Iron Man",
+                FirstName = "Stark",
+                LastName = "Tony",
+                Place = "New York city"
+            }
+        };
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllSuperHero()
+        {
+            return Ok(superHeroes);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetSuperHero(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null)
+                return NotFound("This hero doesnot exist");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddSuperHero(SuperHero superHero)
+        {
+            superHeroes.Add(superHero);
             return Ok(superHero);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSuperHero(int id, SuperHero superHero)
+        {
+            var targetHero = superHeroes.Find(x => x.Id == id);
+            if (targetHero is null)
+            {
+                return NotFound("This hero doesnot exist");
+            }
+            targetHero.FirstName = superHero.FirstName;
+            targetHero.LastName = superHero.LastName;
+            targetHero.Name = superHero.Name;
+            targetHero.Place = superHero.Place;
+
+            return Ok(targetHero);
         }
     }
 }
